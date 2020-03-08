add7542152a188f7798a9331921688936676d15a
kstam <kstam@JamajuPC>
2020-03-08 22:13:27 +0200

    Changes in GUI class Added Countries class Added GUI instance creation in Main for GUI

--- src/pkg3hge/Main.java
+++ src/pkg3hge/Main.java
@@ -15,7 +15,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Δημιουργία της διεπαφής χρήστη
+        GUI gui = new GUI();
+        // Εμφάνιση της διεπαφής χρήστη
+        gui.setVisible(true);        
     }
     
 }
